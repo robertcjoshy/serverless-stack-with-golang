64952d9c1133b231217bb0faed4965a6af2cc4ff
robertcjoshy <robertcjoshy11@gmail.com>
2024-04-11 23:52:33 +0530

    code

--- src/models/db.go
+++ src/models/db.go
@@ -4,16 +4,6 @@ import "gorm.io/gorm"
 
 type Startup struct {
 	gorm.Model
-	Name             string  `json:"name" bson:"name"`
-	CEO              string  `json:"ceo" bson:"ceo"`
-	ValueProposition string  `json:"value_proposition" bson:"value_proposition"`
-	Industry         string  `json:"industry" bson:"industry"`
-	Founder          Founder `json:"founder" bson:"founder" gorm:"embedded"`
-}
-
-type Founder struct {
-	gorm.Model
-	Name       string `json:"name" bson:"name"`
-	Profession string `json:"profession" bson:"profession"`
-	Age        int    `json:"age" bson:"age"`
+	Email    string `json:"email" bson:"email"`
+	Password string `json:"password" bson:"password"`
 }
