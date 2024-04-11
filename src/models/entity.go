c3cbbfbfd70d2bd9bc8d6e2e685d64ee84f24ac8
robertcjoshy <robertcjoshy11@gmail.com>
2024-04-11 23:52:45 +0530

    code

--- src/models/entity.go
+++ src/models/entity.go
@@ -3,31 +3,41 @@ package models
 import (
 	"fmt"
 
+	_ "github.com/lib/pq"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+const (
+	host     = "database-robert.chsu6q8eqfk8.ap-south-1.rds.amazonaws.com"
+	port     = 5432
+	user     = "postgres"
+	password = "daisyjoshy"
+	dbname   = "robertfirst"
+)
+
 var Database *gorm.DB
 
-func OpenDatabaseConnection() {
+func init() {
 	var err error
-	host := "database-robert.chsu6q8eqfk8.ap-south-1.rds.amazonaws.com"
-	username := "postgres"
-	password := "12345678"
-	databaseName := ""
-	port := "5432"
 
-	dsn := fmt.Sprintf("host=%s,user=%s password=%s dbname=%s,port=%s", host, username, password, databaseName, port)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s", host, port, user, password)
 
-	Database, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
+	Database, err = gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
 	if err != nil {
 		panic(err)
-	} else {
-		fmt.Println("connected succesfully")
 	}
 }
 
-func AutoMigrateModels() {
-	Database.AutoMigrate(&Startup{})
+func SaveEmail(mail string) (*Startup, error) {
+	var startup Startup
+	err := Database.Model(&startup).Create(&startup).Error
+	if err != nil {
+		return nil, err
+	}
+	return &startup, err
+}
+
+func GetEmail(search_mail string) (*Startup, error) {
+
 }
